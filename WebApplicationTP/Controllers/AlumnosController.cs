a834ac6e42679067b5f52df1f6b87c6c392251ae
mauri <mauri@LAPTOPDEMAU>
2021-05-25 18:02:02 -0500

    VERSION_1.0

---
+++ WebApplicationTP/Controllers/AlumnosController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplicationTP.Models;
+
+namespace WebApplicationTP.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        // GET: Alumnos
+        public ActionResult Index()
+        {
+            List<AlumnosCLS> listaAlumnos = null;
+
+            using (var bd = new DBAlumnosEntities())
+            {
+                listaAlumnos = (from alumno in bd.Alumnos
+                                select new AlumnosCLS
+                                {
+                                    alumnoid = alumno.CodigoAlumno,
+                                    apellidos = alumno.Apellido,
+                                    nombres = alumno.Nombre,
+                                    escuela = alumno.Escuela
+                                }).ToList();
+
+            }
+
+            return View(listaAlumnos);
+        }
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(AlumnosCLS oAlumnosCLS)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(oAlumnosCLS);
+            }
+            else
+            {
+                using (var bd = new DBAlumnosEntities())
+                {
+                    Alumnos oAlumnos = new Alumnos();
+                    oAlumnos.CodigoAlumno = oAlumnosCLS.alumnoid;
+                    oAlumnos.Nombre = oAlumnosCLS.nombres;
+                    oAlumnos.Apellido = oAlumnosCLS.apellidos;
+                    oAlumnos.Escuela = oAlumnosCLS.escuela;
+                    bd.Alumnos.Add(oAlumnos);
+                    bd.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
+   
---
+++ WebApplicationTP/Models/AlumnosCLS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationTP.Models
+{
+    public class AlumnosCLS
+    {
+        [Key]
+        [Display(Name = "IdAlumno")]
+        public int alumnoid { get; set; }
+
+        [Display(Name = "Apellidos")]
+        [Required]
+        [StringLength(100, ErrorMessage = "Longitud Maxima alcanzada")]
+        public string apellidos { get; set; }
+
+        [Display(Name = "Nombres")]
+        [Required]
+        public string nombres { get; set; }
+
+        [Display(Name = "Escuela Profesional")]
+        [Required]
+        public string escuela { get; set; }
+    }
+}
